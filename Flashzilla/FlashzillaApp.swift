f10cd2244c703c4278ce8905c0e3aeaa542912c0
Henny2 <henrieke.baunack@web.de>
2024-03-09 16:26:13 -1000

    Initial Commit

---
+++ Flashzilla/FlashzillaApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashzillaApp.swift
+//  Flashzilla
+//
+//  Created by Henrieke Baunack on 3/9/24.
+//
+
+import SwiftUI
+
+@main
+struct FlashzillaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
